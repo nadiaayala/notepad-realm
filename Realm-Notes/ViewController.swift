b9c2c0d761fcb67f9dc589749e0ffed41dd56b9e
Nadia <nadiaayalanogueira@outlook.com>
2019-05-24 20:50:52 +0400

    Initial Commit

---
+++ Realm-Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm-Notes
+//
+//  Created by Nadia Ayala on 24/05/19.
+//  Copyright © 2019 Nadia Ayala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
